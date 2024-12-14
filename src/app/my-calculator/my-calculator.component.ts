b24c6925ff5cb616b919650ae87056e413bca175
mingrow <nikitosvozh@gmail.com>
2024-12-14 09:54:17 +0300

    Simple calculator component

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -9,8 +9,10 @@ export class MyCalculatorComponent {
   public firstOperand: number = 0;
   public secondOperand: number = 0;
   public selectOperation: string = '+';
-  public operations: string[] = ['+', '-', '*', '/'];
+  public operations: string[] = ['+', '-', '*', '/', 'sin', 'cos', 'pow'];
+  public history: string[] = [];
   public result: number | undefined = undefined;
+
   public calc() {
     if (this.firstOperand !== undefined && this.secondOperand !== undefined) {
       switch(this.selectOperation) {
@@ -24,10 +26,40 @@ export class MyCalculatorComponent {
           this.result = this.firstOperand * this.secondOperand;
           break;
         } case '/': {
-          this.result = this.firstOperand / this.secondOperand;
+          if (this.secondOperand != 0) {
+            this.result = this.firstOperand / this.secondOperand;
+          }
+          break;
+        } case 'cos': {
+          this.result = Math.cos(this.firstOperand);
+          break;
+        } case 'sin': {
+          this.result = Math.sin(this.firstOperand);
+          break;
+        } case 'pow': {
+          this.result = Math.pow(this.firstOperand, this.secondOperand);
           break;
         }
       }
+      this.writeOperation(this.firstOperand, this.secondOperand, this.result, this.selectOperation);
+      this.firstOperand = 0;
+      this.secondOperand = 0;
     }
+  }
+
+  private writeOperation(first: number, second: number, result: number | undefined, operation: string): void {
+    if (this.history.length >= 5) {
+      this.history.shift();
+    }
+    if (['sin', 'cos'].includes(operation)) {
+      this.history.push(`${operation}${first} = ${result}`)
+    } else {
+      this.history.push(`${first} ${operation} ${second} = ${result}`);
+    }
+
+  }
+
+  public isOrdinaryOperations(): boolean {
+    return ['sin', 'cos'].includes(this.selectOperation);
   }
 }
